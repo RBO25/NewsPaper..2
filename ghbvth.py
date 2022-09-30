db42862e5fbe8bd2fa346ceb533a63c5a166f95b
RBO25 <107694089+RBO25@users.noreply.github.com>
2022-09-30 17:20:28 +0500

    Update ghbvth.py

--- ghbvth.py
+++ ghbvth.py
@@ -9,14 +9,3 @@
 #
 # print(censor('редиска нехороший человек', str))
 
-def censor(value):
-    """
-    The function replaces bad words specified in the list a[].
-    Example: 'хрен' ->> 'x***'
-    """
-    if isinstance(value, str):
-        a = ['хрен', 'редиска', 'тыква']
-        for i in a:
-            b = ' '+i[0]+'*' * (len(i)-1)+' '
-            value = value.replace(i, b)
-    return value
